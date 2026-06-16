4bd0c57fca3b9ecab7c9b33b3999d4516f7f0ce0
Jainil <mcalts.nfas@gmail.com>
2026-06-16 19:17:57 +0100

    fix games folder structure

--- script.js
+++ script.js
@@ -5,17 +5,7 @@ search.addEventListener("input", () => {
   const value = search.value.toLowerCase();
 
   cards.forEach(card => {
-    const text = card.textContent.toLowerCase();
-    card.style.display = text.includes(value) ? "flex" : "none";
+    const name = card.dataset.name.toLowerCase();
+    card.style.display = name.includes(value) ? "flex" : "none";
   });
 });
-
-function filterGames(type) {
-  cards.forEach(card => {
-    if (type === "all") {
-      card.style.display = "flex";
-    } else {
-      card.style.display = card.classList.contains(type) ? "flex" : "none";
-    }
-  });
-}
